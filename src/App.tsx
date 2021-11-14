69438f94af8ba83471ae6c677a7fa690ca71ebf9
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2021-11-14 11:32:02 +0900

    authentication test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useRef } from "react";
+import { Button, Col, Container, Form, Navbar } from "react-bootstrap";
+import { AuthContext } from "./context/AuthContext";
+import { auth } from "./firebaseSetup";
 
 function App() {
+  const user = useContext(AuthContext);
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const createAccount = async () => {
+    try {
+      await auth.createUserWithEmailAndPassword(
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signIn = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signOut = async () => {
+    await auth.signOut();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar className="justify-content-between" bg="dark" variant="dark">
+        <Navbar.Brand>Firebase Authentication</Navbar.Brand>
+        {user && <Button onClick={signOut}>Sign Out</Button>}
+      </Navbar>
+      {!user ? (
+        <Container style={{ maxWidth: "500px" }} fluid>
+          <Form className="mt-4">
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control ref={emailRef} type="email" placeholder="email" />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                ref={passwordRef}
+                type="password"
+                placeholder="password"
+              />
+            </Form.Group>
+            <Form>
+              <Col xs={6}>
+                <Button onClick={createAccount} type="button">
+                  Sign Up
+                </Button>
+              </Col>
+              <Col xs={6}>
+                <Button
+                  onClick={signIn}
+                  type="button"
+                  variant="secondary"
+                >
+                  Sign In
+                </Button>
+              </Col>
+            </Form>
+          </Form>
+        </Container>
+      ) : (
+        <h2 className="mt-4 text-center">Welcome {user.email}</h2>
+      )}
+    </>
   );
 }
 
